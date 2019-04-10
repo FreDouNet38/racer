bbc705ba9687f9ed17f9163fee435b43f79c16d2
FreDouNet <frederic.berto38@gmail.com>
2019-04-10 18:20:37 +0200

    I created the classes Pilot and Car. The program can show a track with 20 trackparts and 5 car/pilot

--- racer.py
+++ racer.py
@@ -6,6 +6,10 @@ TERRAINS = ["asphalt", "sand", "mud", "rocky"]
 COMPLEXITIES = ["normal", "rapid", "subtle"]
 
 list_parts = []
+list_car_pilots = []
+
+print("              Welcome, let's plays racer!!\n\n")
+
 
 class TrackPart:
     """This class is going to generate a part of the final racetrack"""
@@ -18,27 +22,68 @@ class TrackPart:
         
    
 
-class Track:
+class Track(TrackPart):
     """This class is adding parts to generate a complete track.
         there are 20 parts originally"""
 
 
-    def __init__(self, parts = 20):
+    def __init__(self):
         TrackPart.__init__(self)
-        self.parts = parts    
+            
 
     def show_part(self):
         return(self.complexity +" "+ self.terrain + " "  + "(" + str(self.length) + ")")
 
     def add_parts(self):
-        for i in range(0, self.parts):
+        for i in range(20):
             list_parts.append(Track())
            
         for elt in list_parts:
             print(elt.show_part(), end = ' + ')
+            
      
 t = Track()
-print(t.add_parts())
+print("Using track:\n")
+t.add_parts()
+
+class Pilot:
+    """This class creates a pilot"""
+
+    def __init__(self):
+        self.name = randchoice(ascii_uppercase)
+        self.normal_speed = uniform(0.5, 1.5)
+        self.rapid_speed = uniform(0.5, 1.5)
+        self.subtle_speed = uniform(0.5, 1.5)
+
+        
+
+class Car(Pilot):
+    """This class creates a car"""
+
+    def __init__(self):
+        Pilot.__init__(self)
+        self.carname = randint(0,20)
+        self.asphalt_speed = uniform(0.5, 1.5)
+        self.sand_speed = uniform(0.5, 1.5)
+        self.mud_speed = uniform(0.5, 1.5)
+        self.rocky_speed = uniform(0.5, 1.5)
+
+    def pilot_car(self):
+        for i in range(5):
+            list_car_pilots.append(Car())
+            
+        for pc in list_car_pilots:
+            print("Car ",pc.carname, "with pilot", pc.name, end = " ; ")
+
+print("\n\nThe contestants are:\n")
+p = Car()
+p.pilot_car()
+            
+
+
+        
+
+    
 
 
 
